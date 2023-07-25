033202f643cca9641cc8ce5ae4a04b87013a04fe
Michel Tricot <michel@airbyte.io>
2023-07-24 20:08:51 -0700

    intial commit

---
+++ src/airbed/tools.py
@@ -0,0 +1,16 @@
+import json
+from json import JSONDecodeError
+
+
+def parse_messages(lines):
+    collect = []
+    for line in lines.splitlines():
+        try:
+            collect.append(json.loads(line))
+        except JSONDecodeError:
+            pass
+    return collect
+
+
+def pp_json(json_object):
+    print(json.dumps(json_object, indent=2))
