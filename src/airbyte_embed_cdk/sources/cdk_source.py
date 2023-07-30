7f8a1b868d037b35ac17398d1c878c8856d433b8
Michel Tricot <michel@airbyte.io>
2023-07-29 17:22:23 -0700

    support BaseModel

--- src/airbyte_embed_cdk/sources/cdk_source.py
+++ src/airbyte_embed_cdk/sources/cdk_source.py
@@ -4,6 +4,7 @@ from typing import Iterable, Optional
 from airbyte_cdk.connector import TConfig
 from airbyte_cdk.sources.source import Source, TState
 from airbyte_protocol.models import AirbyteMessage, ConfiguredAirbyteCatalog, Type
+from pydantic import BaseModel
 
 from airbyte_embed_cdk.models.source import SourceRunner
 
@@ -18,12 +19,18 @@ class CdkSourceRunner(SourceRunner):
         yield AirbyteMessage(type=Type.SPEC, spec=spec)
 
     def check(self, config: TConfig) -> Iterable[AirbyteMessage]:
+        if isinstance(config, BaseModel):
+            config = config.dict()
         status = self._source.check(self._logger, config)
         yield AirbyteMessage(type=Type.CONNECTION_STATUS, connectionStatus=status)
 
     def discover(self, config: TConfig) -> Iterable[AirbyteMessage]:
+        if isinstance(config, BaseModel):
+            config = config.dict()
         catalog = self._source.discover(self._logger, config)
         yield AirbyteMessage(type=Type.CATALOG, catalog=catalog)
 
     def read(self, config: TConfig, catalog: ConfiguredAirbyteCatalog, state: Optional[TState]) -> Iterable[AirbyteMessage]:
+        if isinstance(config, BaseModel):
+            config = config.dict()
         yield from self._source.read(self._logger, config, catalog, state)  # type: ignore
